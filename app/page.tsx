9a10ab113f436c82e01200e119956cb2f61fc410
Michael Salam <michaeldimimu@gmail.com>
2024-11-30 09:56:11 +0100

    Create default text and heading styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Vicsmall</h1>;
+  return (
+    <>
+      <h1>Vicsmall</h1> <h2>Flash sales</h2> <h3>Male shoes</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit minima,
+        harum perferendis modi quasi necessitatibus nam voluptates dolor ducimus
+        perspiciatis incidunt asperiores et tempore ea quos porro saepe
+        accusamus aperiam?
+      </p>
+    </>
+  );
 }
