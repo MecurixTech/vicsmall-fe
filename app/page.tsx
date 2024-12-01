d7de735a091685a8f094067442f23e6a710b6061
Michael Salam <michaeldimimu@gmail.com>
2024-12-01 21:48:01 +0100

    Create promotional banner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import CategoryCard from "./components/home/category-card";
 import ProductCard from "./components/product-card";
+import Banner from "./components/home/banner";
+
 import { categories, products } from "./data/dummyData";
 
 export default function Home() {
   return (
-    <main className="p-4">
+    <main className="mx-auto w-[90%]">
+      <Banner />
       <section className="grid grid-cols-5 gap-4">
         {products.map((product) => (
           <ProductCard key={product.id} {...product} />
