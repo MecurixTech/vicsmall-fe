973d962a507f31581c85f9fde7b50832571079b2
Edidiong Harrison <edidiongharrison18@gmail.com>
2024-12-21 21:00:44 +0100

    created review page for view more button

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -8,9 +8,7 @@ import { ArrowLeft } from "@mui/icons-material";
 const ReviewSection = ({ reviews }: { reviews: review[] }) => {
   return (
     <div className="pt-4">
-  
-      <div className="sticky top-0 z-10 bg-slate-100 mb-4 flex items-center gap-2 px-4">
-     
+      <div className="sticky top-0 z-10 mb-4 flex items-center gap-2 bg-slate-100 px-4">
         <button
           onClick={() => window.history.back()}
           className="text-gray-600 hover:text-gray-900"
@@ -18,13 +16,11 @@ const ReviewSection = ({ reviews }: { reviews: review[] }) => {
           <ArrowLeft className="text-4xl" />
         </button>
 
-   
         <div className="flex-1 text-center">
           <h1 className="text-4xl font-extrabold text-gray-800">Reviews</h1>
         </div>
       </div>
 
-     
       <div className="px-4">
         <Reviews reviews={reviews} />
       </div>
