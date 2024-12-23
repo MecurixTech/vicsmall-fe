368d8c0fe5c861aa5bb92ad3cdded1de9469d64a
Michael Salam <michaeldimimu@gmail.com>
2024-12-23 11:12:19 +0100

    Add footer component to notifications page

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/footer";
 import Notification from "../components/notifications/notification";
 import { notifications } from "../data/dummyData";
 
@@ -6,11 +7,13 @@ const NotificationsPage = () => {
     <>
       <h1 className="my-8 text-center text-3xl">Notifications</h1>
 
-      <div className="mx-auto mb-8 w-[95%] rounded-xl bg-white p-8 shadow-lg sm:w-4/5">
+      <div className="mx-auto mb-16 w-[95%] rounded-xl bg-white p-8 shadow-lg sm:w-4/5">
         {notifications.map((notification) => (
           <Notification key={notification.id} notification={notification} />
         ))}
       </div>
+
+      <Footer />
     </>
   );
 };
