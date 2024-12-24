e7063bc31003165f433fff0932fa0ea7bb4c2645
Michael Salam <michaeldimimu@gmail.com>
2024-12-24 19:04:19 +0100

    Remove extra navbar from product page

--- app/product-page/page.tsx
+++ app/product-page/page.tsx
@@ -1,25 +1,20 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 
-import ProductSettings from '../components/product-page/product-settings'
-import InfoTabs from '../components/product-page/info-tabs-container'
-import {  products } from "../data/dummyData";
-import Footer from '../components/footer'
-import Navbar from '../components/navbar'
-import ProductCard from '../components/product-card/product-card'
+import ProductSettings from "../components/product-page/product-settings";
+import InfoTabs from "../components/product-page/info-tabs-container";
+import { products } from "../data/dummyData";
+import Footer from "../components/footer";
+import ProductCard from "../components/product-card/product-card";
 
 const page = () => {
-  
-  
   return (
     <div>
-      <Navbar />
-        <div className="flex items-start justify-between gap-4">
-        
+      <div className="flex items-start justify-between gap-4">
         <ProductSettings />
       </div>
-     
+
       <InfoTabs />
 
       <hr className="mb-8" />
@@ -33,10 +28,9 @@ const page = () => {
         </div>
       </section>
 
-      
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
