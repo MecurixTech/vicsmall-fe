aba1df4242bd5dab9d902466a4ea66b467707db9
Syrillings <sinenernest43@gmail.com>
2025-02-27 20:45:43 +0100

    Selecting Customer Preference

--- app/components/signup/form/step-one.tsx
+++ app/components/signup/form/step-one.tsx
@@ -25,7 +25,19 @@ const stepOneValidationSchema = Yup.object({
     .oneOf([Yup.ref("password")], "Passwords must match"),
 });
 
-const StepOne = (props) => {
+interface StepOneProps {
+  data: {
+    email: string;
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+    password: string;
+    confirm_password: string;
+  };
+  next: (values: any, isFinalStep: boolean) => void;
+}
+
+const StepOne: React.FC<StepOneProps> = (props) => {
   const [isShowingPassword, setIsShowingPassword] = useState(false);
   const [isShowingConfirmPassword, setIsShowingConfirmPassword] = useState(false);
   const [loading] = useState(false);
