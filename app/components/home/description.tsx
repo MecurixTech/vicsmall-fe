bf859515170f83767e8b01ea166a9990ea93f8ec
Syrillings <sinenernest43@gmail.com>
2025-02-28 22:59:00 +0100

    Mobile Responsiveness

--- app/components/home/description.tsx
+++ app/components/home/description.tsx
@@ -14,7 +14,7 @@ const DescriptionSection = () => {
 
 
                 <p className="text-[13px] font-normal leading-[14px] text-[rgba(30,30,30,0.72)]">
-                Vicsmall is a vibrant online marketplace where buyers and sellers converge to exchange goods and services. With a vast array of products on offer, Vicsmall is poised to revolutionize the e-commerce landscape. Sellers can showcase their wares, from unique handmade items to brand-name goods, in a secure and user-friendly environment. Meanwhile, buyers can browse and purchase with confidence, thanks to Vicsmall's robust trust and safety measures. Whether you're a seasoned entrepreneur or an occasional seller, Vicsmall provides the perfect platform to connect with customers and grow your business. With its intuitive interface and commitment to excellence, Vicsmall is the go-to destination for online shoppers and sellers alike.
+                Vicsmall is a vibrant online marketplace where buyers and sellers converge to exchange goods and services. With a vast array of products on offer, Vicsmall is poised to revolutionize the e-commerce landscape. Sellers can showcase their wares, from unique handmade items to brand-name goods, in a secure and user-friendly environment. Meanwhile, buyers can browse and purchase with confidence, thanks to Vicsmall&apos;ss robust trust and safety measures. Whether you&apos;sre a seasoned entrepreneur or an occasional seller, Vicsmall provides the perfect platform to connect with customers and grow your business. With its intuitive interface and commitment to excellence, Vicsmall is the go-to destination for online shoppers and sellers alike.
                 </p>
             </div>
 
